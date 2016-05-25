1194d755827cf0974fbbd61b09fe909d9a6a5406
HugoSilveiro <hugofilipe95@hotmail.com>
2016-05-25 17:08:22 +0100

    NaN NO MORE - SOMETIMES

--- Assets/Scripts/StatisticsLogger.cs
+++ Assets/Scripts/StatisticsLogger.cs
@@ -22,19 +22,31 @@ public class StatisticsLogger {
 	//saves fitness info and writes to console
 	public void PostGenLog(List<Individual> pop, int currentGen) {
 		pop.Sort((x, y) => x.fitness.CompareTo(y.fitness));
-		
-		if(float.IsNaN(pop[0].fitness)){
-			Debug.Log("nan");
+		int i = 0;
+		while(float.IsNaN(pop[i].fitness) && i < pop.Count-1){
+				if(!float.IsNaN(pop[i+1].fitness)){
+					pop[0].fitness = pop[i+1].fitness;
+					break;
+				}
+				else{
+					i++;
+				}
 		}
 		Debug.Log("fitness value: "+pop[0].fitness);
 
 		bestFitness.Add (currentGen, pop[0].fitness);
 		meanFitness.Add (currentGen, 0f);
-
+		int NaNNumbers = 0;
 		foreach (Individual ind in pop) {
-			meanFitness[currentGen]+=ind.fitness;
+			if(!float.IsNaN(ind.fitness)){
+					meanFitness[currentGen]+=ind.fitness;
+			}
+			else{
+				NaNNumbers++;
+			}
+
 		}
-		meanFitness [currentGen] /= pop.Count;
+		meanFitness [currentGen] /= (pop.Count - NaNNumbers);
 
 		Debug.Log ("generation: "+currentGen+"\tbest: " + bestFitness [currentGen] + "\tmean: " + meanFitness [currentGen]+"\n");
 	}
